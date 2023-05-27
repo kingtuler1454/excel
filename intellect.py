6fb1bf2d5055398a729c7818257861fa32d2f968
kingtuler1454 <morozovsergey.03@gmail.com>
2023-05-28 02:50:43 +0400

    работает соединение в единую базу

--- intellect.py
+++ intellect.py
@@ -3,77 +3,13 @@ import openpyxl as op
 from tqdm import trange  # для отображения прогресса в консоли
 from progress.bar import IncrementalBar  # для отображения прогресса в консоли
 
-
-def read_excel(filename: str):  # функция принимающая имя эксель таблицы
-    wb = op.load_workbook(filename, data_only=True)
-    sheet = wb.active
-    
-    information = []  # список информации о студентах
-    for i in range(1, sheet.max_column + 1): information.append(sheet.cell(row=1, column=i).value)
-    for student_number in trange(2, sheet.max_row + 1):  # бежим по каждой строчке
-        column_names = []  # список имён столбцов
-        column_students = []  # список значений студента в этих столбцах
-        for i in range(1, sheet.max_column + 1):  # бежим по каждому столбцу
-            column_students.append(sheet.cell(row=student_number, column=i).value)  # добавляем в список значений студента текущее значение студента
-        information.append(dict(zip(column_names, column_students)))  # создаем из списка имён столбцов и список значений студента словарик  и добавляем каждый словарик в список information
-    return information  # вовзращаем список из словарей
-
-def del_None(information):
-    tmp=[]
-    for elem in information:
-        tmp.append({key:val for key,val in elem.items() if val != None  and val!=''})
-    return tmp
-
-def sum(list1, list2):  # функция сложения двух списков из двух эксель таблиц
-    list1=uniq_list(list1)
-    list2=uniq_list(list2)
-    
-    bar = IncrementalBar( "Countdown", max=len(list1) )  # для отображения прогресса в консоли
-    tmp2=list2
-    for elem1 in list1:  # для каждого элемента из первой таблицы
-        for elem2 in list2:  # для каждого элемента из второйтаблицы
-            # теперь проверяем есть ли среди двух таблиц повторяющийся студент
-            if ( elem1.get("Дата рождения").lower() == elem2.get("Дата рождения").lower()):  # сначала проверяем равны ли даты рождения,
-                if (elem1.get("Фамилия").lower() == elem2.get("Фамилия").lower()):  # если даты равны то затем фамилии,
-                    if elem1.get("Отчество").lower() == elem2.get("Отчество").lower():
-                        if ( elem1.get("Имя").lower() == elem2.get("Имя").lower()):  # ну если и имена равны то это явно один и тот же человек
-                            value = set(elem2) - set(elem1)  # смотрим разницу между словарями. получитс список ключей второго словаря, которых нет в первом
-                            for new_key in list(value):  # для каждого такого ключа добавляем занчение  в первый словарь
-                                elem1[new_key] = elem2.get(new_key)
-                            tmp2.remove(elem2) #удаляем значения одинаковых студентов из копии списка2
-        bar.next()  # для отображения прогресса в консоли
-
-    return list1+tmp2  # возвращаем дополненный первый список
-
-
-def uniq_list(information):
-    list1=[]
-    for elem in information:
-        tmp={key:val for key,val in elem.items() if val != None  and val!=''}
-        if (len(tmp))!=0:list1.append(tmp) # удаляем значения у словарей где None 
-
-    for i in range (len(list1)-1):
-        for j in range (1,len(list1)-1):
-            if list1[i].get("Дата рождения")==list1[j].get("Дата рождения"):
-                if list1[i].get("Имя")==list1[j].get("Имя"):
-                    if list1[i].get("Фамилия")== list1[j].get("Фамилия"):
-                        if list1[i].get("Отчество")== list1[j].get("Отчество"):
-                            if len(list1[i])<=len(list1[j]):
-                                value = set(list1[j]) - set(list1[i])  # смотрим разницу между словарями. получитс список ключей второго словаря, которых нет в первом
-                                for new_key in list(value):  # для каждого такого ключа добавляем занчение  в первый словарь
-                                    list1[i][new_key] = list1[j].get(new_key)
-                            list1.pop(j)
-
-    return list1
-
-
-def read_officer(filename: str): # 44 столбца заранее известных
+def read_cicle(filename: str, Solder:bool): # 44 столбца заранее известных
 
     information = []  # список информации о студентах
     column_names = [
         "ФГОО ВО в котором обучается студент",#1
         "ФГОО ВО при которой создан ВУЦ",#2
-        "ВУС",#3
+        "ВУС",#3 
         "ОВУ",#4
         "ФГОС",#5
         "Программа военной подготовки",#6
@@ -116,16 +52,81 @@ def read_officer(filename: str): # 44 столбца заранее извест
         "Приказ о отчислении",	#43
         "Причина отчисления"    #44																							
     ]
-    
+    if Solder: column_names.insert(2,"Код должности")
     wb = op.load_workbook(filename, data_only=True)
     sheet = wb.active
-   
-    
     for student_number in trange (5,sheet.max_row + 1):
+        if sheet.cell(row=student_number, column=1).value==None:break
         column_students = []  # список значений студента в этих столбцах
-        for i in range(1,45):column_students.append(sheet.cell(row=student_number, column=i).value )
+        for i in range(1,sheet.max_column + 1):column_students.append(sheet.cell(row=student_number, column=i).value )
         information.append(dict(zip(column_names, column_students)))  # создаем из списка имён столбцов и список значений студента словарик  и добавляем каждый словарик в список information  
     return information
+
+def read_excel(filename: str):  # функция принимающая имя эксель таблицы
+    wb = op.load_workbook(filename, data_only=True)
+    sheet = wb.active
+    if sheet.cell(row=1, column=4).value=="Код должности":
+        return read_cicle(filename,True)
+    if sheet.cell(row=1, column=4).value=="ОВУ":
+        return read_cicle(filename,False)
+
+    information = []  # список информации о студентах
+    column_names = []  # список имён столбцов
+    for i in range(1, sheet.max_column + 1): column_names.append(sheet.cell(row=1, column=i).value)
+    for student_number in trange(2, sheet.max_row + 1):  # бежим по каждой строчке
+        column_students = []  # список значений студента в этих столбцах
+        for i in range(1, sheet.max_column + 1):  # бежим по каждому столбцу
+            column_students.append(sheet.cell(row=student_number, column=i).value)  # добавляем в список значений студента текущее значение студента
+        information.append(dict(zip(column_names, column_students)))  # создаем из списка имён столбцов и список значений студента словарик  и добавляем каждый словарик в список information
+    return information  # вовзращаем список из словарей
+
+def sum(list1, list2):  # функция сложения двух списков из двух эксель таблиц
+
+    list1=uniq_list(list1)
+    list2=uniq_list(list2)
+    
+    bar = IncrementalBar( "Countdown", max=len(list1) )  # для отображения прогресса в консоли
+    tmp2=list2
+    for elem1 in list1:  # для каждого элемента из первой таблицы
+        for elem2 in list2:  # для каждого элемента из второйтаблицы
+            # теперь проверяем есть ли среди двух таблиц повторяющийся студент
+            if ( elem1.get("Дата рождения") == elem2.get("Дата рождения")):  # сначала проверяем равны ли даты рождения,
+                if (elem1.get("Фамилия").lower() == elem2.get("Фамилия").lower()):  # если даты равны то затем фамилии,
+                    if elem1.get("Отчество").lower() == elem2.get("Отчество").lower():
+                        if ( elem1.get("Имя").lower() == elem2.get("Имя").lower()):  # ну если и имена равны то это явно один и тот же человек
+                            value = set(elem2) - set(elem1)  # смотрим разницу между словарями. получитс список ключей второго словаря, которых нет в первом
+                            for new_key in list(value):  # для каждого такого ключа добавляем занчение  в первый словарь
+                                elem1[new_key] = elem2.get(new_key)
+                            tmp2.remove(elem2) #удаляем значения одинаковых студентов из копии списка2
+        bar.next()  # для отображения прогресса в консоли
+    return list1+tmp2  # возвращаем дополненный первый список
+
+def uniq_list(information):
+    list1=[]
+    for elem in information:
+        tmp={key:val for key,val in elem.items() if val != None  and val!=''}
+        if (len(tmp))!=0:list1.append(tmp) # удаляем значения у словарей где None 
+
+    tmp=[]
+    for i in range (len(list1)-1):
+        for j in range (i+1,len(list1)-2):
+            if list1[i].get("Дата рождения")==list1[j].get("Дата рождения"):
+                print(list1[i].get("Дата рождения"))
+                print(list1[j].get("Дата рождения"))
+                if list1[i].get("Имя")==list1[j].get("Имя"):
+                    if list1[i].get("Фамилия")== list1[j].get("Фамилия"):
+                        if list1[i].get("Отчество")== list1[j].get("Отчество"):
+                            if len(list1[i])<=len(list1[j]):
+                                value = set(list1[j]) - set(list1[i])  # смотрим разницу между словарями. получитс список ключей второго словаря, которых нет в первом
+                                for new_key in list(value):  # для каждого такого ключа добавляем занчение  в первый словарь
+                                    list1[i][new_key] = list1[j].get(new_key)
+                            tmp.append(j)
+    for i in range(len(tmp)):
+        list1.pop(tmp[i]-i)
+
+    return list1
+#для суммирования вспомогаюзая
+
 
 if __name__ == "__main__":
      #read_officer читает
@@ -134,10 +135,12 @@ if __name__ == "__main__":
 
     # list1 = read_excel("Солдаты их БД ВУЦ.xlsx")
     # print(list1)
-     with open("tmp.txt", mode="w+") as file:
-        tmp=read_officer('офиц.xlsx')
-        
-    
+    tmp1=read_excel('Из Паруса сведения (1).xlsx')
+    with open("tmp.txt", mode="w+") as file:
+        # tmp1=read_cicle('солдаты.xlsx',True)
+        tmp2=read_cicle('5 цикл офиц Морозов2 (1).xlsx',False)
+        file.write(str(sum(tmp1,tmp2)))
+
 
     # list1=uniq_list(read_excel( 'офиц.xlsx'))
     # with open("tmp.txt", mode="w+") as file:
